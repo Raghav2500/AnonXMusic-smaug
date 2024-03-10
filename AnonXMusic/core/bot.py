847792862475a79ebef5b63856a99512b6ef3a37
Radium89 <157292203+Radium89@users.noreply.github.com>
2024-03-10 14:38:26 +0530

    Update bot.py
    Signed-off-by: Radium89 <157292203+Radium89@users.noreply.github.com>

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -1,14 +1,11 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
+
 import config
+
 from ..logging import LOGGER
-proxy = {
-    "scheme": "http",  # "socks4", "socks5" and "http" are supported
-    "hostname": "proxy.proxyverse.io",
-    "port": 9200,
-    "username": "country-in",
-    "password": "9b01816d-048c-42e7-8686-411f91665d28"
-}
+
+
 class Anony(Client):
     def init(self):
         LOGGER(name).info(f"Starting Bot...")
@@ -17,31 +14,34 @@ class Anony(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            proxy=proxy,
             in_memory=True,
+            parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
         )
+
     async def start(self):
         await super().start()
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
+
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>   {self.mention}  ^y  ^o  ^{ s  ^{  ^ ^  ^{  ^g  ^e :<>
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(name).error(
-                "Bot has failed to access the log group/channel. Make sure that you ha>
+                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
             exit()
         except Exception as ex:
             LOGGER(name).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex>
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).name}."
             )
             exit()
+
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(name).error(
@@ -49,6 +49,6 @@ class Anony(Client):
             )
             exit()
         LOGGER(name).info(f"Music Bot Started as {self.name}")
+
     async def stop(self):
         await super().stop()
-`
