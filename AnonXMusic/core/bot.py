ffe996183d8f6c517b40cf43ba9340af3a3d44a6
Mohitag403 <mohitag403@gmail.com>
2024-03-10 16:38:02 +0530

    Revert "Update bot.py"
    This reverts commit 4485a56804720b65effc4df8f8cfa083e731452a.

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -5,13 +5,13 @@ import config
 
 from ..logging import LOGGER
 
-proxy = (
+proxy = {
     "scheme": "http",  # "socks4", "socks5" and "http" are supported
     "hostname": "proxy.proxyverse.io",
     "port": 9200,
     "username": "country-in",
     "password": "9b01816d-048c-42e7-8686-411f91665d28"
-)
+}
 
 
 class Anony(Client):
